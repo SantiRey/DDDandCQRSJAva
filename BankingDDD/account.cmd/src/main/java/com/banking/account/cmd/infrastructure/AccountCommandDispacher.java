2c2d8a4ec5b362b6218f75b6c36dbd2dd5c66f00
kevin santiago rey rodriguez <kev_rey@uni-bremen.de>
2022-05-22 23:19:17 +0200

    Mediator Design pattern

---
+++ BankingDDD/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountCommandDispacher.java
@@ -0,0 +1,27 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.banking.cqrs.core.commands.BaseCommand;
+import com.banking.cqrs.core.commands.CommandHandlerMethod;
+import com.banking.cqrs.core.infrastructure.CommandDispacher;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class AccountCommandDispacher implements CommandDispacher {
+    private final Map<Class<? extends BaseCommand>, List<CommandHandlerMethod>> routes = new HashMap<>();
+
+    @Override
+    public <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler) {
+        List<CommandHandlerMethod> handlers = routes.computeIfAbsent(type, c -> new LinkedList<>());
+        handlers.add(handler);
+    }
+
+    @Override
+    public void send(BaseCommand command) {
+       var handlers =  routes.get(command.getClass());
+       if(handlers == null || handlers.size() != 1)
+           throw new RuntimeException("handler no apropiado");
+       handlers.get(0).handle(command);
+    }
+}
--- BankingDDD/readme.md
+++ BankingDDD/readme.md
@@ -3,3 +3,6 @@ Messages types:
 -> Commands -> exectu action
 -> Events -> change observables (cambian el estado de aplicacion, verbo en pasado)
 -> Queries -> 
+
+Patron Mediator interface que describe una comunicacion entre objetos
+-> Mediador es una interface 
